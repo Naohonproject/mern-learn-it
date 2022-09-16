c1a0d43c1a0e85dc97b521c78312948df56f9b0d
LE TUAN BAO <LETUANBAO@mbg8.onmicrosoft.com>
2022-09-16 23:49:05 +0700

    add PUT /posts/:id api to update indicated post

--- server/routes/post.js
+++ server/routes/post.js
@@ -10,7 +10,9 @@ const verifyToken = require("../middleware/auth");
 router.get("/", verifyToken, async (req, res) => {
   try {
     //   try to take all post of current user in database
-    const posts = await Post.find({ user: req.userId });
+    const posts = await Post.find({ user: req.userId }).populate("user", [
+      "userName",
+    ]);
     //   if posts exist, Post.find() eventually fullfil, res
     res.json({ success: true, posts });
   } catch (error) {
@@ -26,8 +28,6 @@ router.get("/", verifyToken, async (req, res) => {
 // @access Private
 router.post("/", verifyToken, async (req, res) => {
   const { title, description, url, status } = req.body;
-
-  console.log(req.body);
 
   // simple validation
   if (!title) {
@@ -54,6 +54,60 @@ router.post("/", verifyToken, async (req, res) => {
       success: true,
       message: "Happy Learning!",
       post: newPost,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+});
+
+// @routes PUT api/posts
+// @desc Update posts
+// @access Private
+
+router.put("/:id", verifyToken, async (req, res) => {
+  const { title, description, url, status } = req.body;
+
+  // simple validation
+  if (!title) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Title is required" });
+  }
+
+  try {
+    //   try to crate a object name updatedPost from req.body
+    let updatedPost = {
+      title,
+      description: description || "",
+      url: (url.startsWith("https://") ? url : `https://${url}`) || "",
+      status: status || "TO LEARN",
+    };
+    // create an object to be as a condition for finding the post in database
+    const postUpdateCondition = { _id: req.params.id, user: req.userId };
+
+    //   find the post with id and user name then update that post
+    updatedPost = await Post.findOneAndUpdate(
+      postUpdateCondition,
+      updatedPost,
+      {
+        new: true,
+      }
+    );
+
+    //   check user not authorized to have right to update
+    if (!updatedPost) {
+      return res.status(401).json({
+        success: false,
+        message: "Post not found or user not unauthorized",
+      });
+    }
+    // if updatedPost exist, means findAndUpdate eventually fulfil,return to client
+    // the json view includes the updated post
+    res.json({
+      success: true,
+      post: updatedPost,
+      message: "Excellent Progress!",
     });
   } catch (error) {
     console.log(error);
